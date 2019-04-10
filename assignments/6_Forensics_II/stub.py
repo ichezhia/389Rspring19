386ae5b69dd16029e9a8d6289ace0042e37e171a
ichezhia <iniyanc@hotmail.com>
2019-04-09 21:28:28 -0400

    assignment 6 files

--- assignments/6_Forensics_II/stub.py
+++ assignments/6_Forensics_II/stub.py
@@ -3,6 +3,7 @@
 import sys
 import struct
 
+from datetime import datetime
 
 # You can use this method to exit on failure conditions.
 def bork(msg):
@@ -12,6 +13,7 @@ def bork(msg):
 # Some constants. You shouldn't need to change these.
 MAGIC = 0x8BADF00D
 VERSION = 1
+SECTION_COUNT = 0   #used to compare to validate
 
 if len(sys.argv) < 2:
     sys.exit("Usage: python stub.py input_file.fpff")
@@ -32,6 +34,15 @@ if magic != MAGIC:
 if version != VERSION:
     bork("Bad version! Got %d, expected %d" % (int(version), int(VERSION)))
 
+
+timestamp, author, section_count = struct.unpack("<L8sL", data[8:24])
+
+#TODO: author validation
+
+#validate section count
+if section_count <= SECTION_COUNT:
+    bork("Bad section count! Got %d, expected greater than %d" % (int(section_count), int(SECTION_COUNT)))
+
 print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
@@ -39,4 +50,17 @@ print("VERSION: %d" % int(version))
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
 
+#convert string to int
+timestamp_int = int(timestamp)
+
+#format Unix time to readable format
+timestamp = datetime.utcfromtimestamp(timestamp_int).strftime('%Y-%m-%d %H:%M:%S')
+
+print("TIMESTAMP: %s" % timestamp)
+
+print("AUTHOR: %s" % author)
+
+print("SECTION COUNT: %s" % section_count)
+
 print("-------  BODY  -------")
+#TODO: BODY
