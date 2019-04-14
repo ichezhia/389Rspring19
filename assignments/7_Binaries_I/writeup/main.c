a4ece9c638c4e932d0665883966eaf5212997922
ichezhia <iniyanc@hotmail.com>
2019-04-13 22:59:37 -0400

    hw7

--- assignments/7_Binaries_I/writeup/main.c
+++ assignments/7_Binaries_I/writeup/main.c
@@ -1,11 +1,22 @@
 /*
- * Name: *PUT YOUR NAME HERE*
- * Section: *PUT YOUR SECTION NUMBER HERE*
+ * Name: Iniyan Chezhian
+ * Section: 0101
  *
  * I pledge on my honor that I have not given or received any unauthorized
  * assistance on this assignment or examination.
  *
- * Digital acknowledgement: *PUT YOUR NAME HERE*
+ * Digital acknowledgement: Iniyan Chezhian
  */
 
-/* your code goes here */
+int foo1 = 485163226
+int foo2 = 4277009102
+
+print a = %d\n, foo2
+print b = %d\n, foo1
+
+foo2^=
+foo1^=
+foo2^=
+
+print foo2
+print foo1
