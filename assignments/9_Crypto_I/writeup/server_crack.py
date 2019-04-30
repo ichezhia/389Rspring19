1647682de8062297523bc97022a90efbe8692309
ichezhia <iniyanc@hotmail.com>
2019-04-29 23:18:13 -0400

    hw9 file

--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -5,18 +5,35 @@ import string
 import socket
 import time
 
-def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+def crack():
+
+    hashes = [line.rstrip() for line in open('hashes.txt','r')]
+    passwords = [line.rstrip() for line in open('passwords.txt','r')]
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
+
+    for c in characters:
+        for p in passwords:
+            brute_string = c+p
+            hash = hashlib.sha256(brute_string).hexdigest()
+
+#check all hashes
+            if hash in hashes:
+                    print("FOUND! character: " + c + " password: " + p + " is hash: " + hash)
+
+def server_crack():
+    hashes = [line.rstrip() for line in open('hashes.txt','r')]
+    passwords = [line.rstrip() for line in open('passwords.txt','r')]
+    characters = string.ascii_lowercase
+    server_ip = '134.209.128.58 1337'
     server_port = 00000
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
     data = s.recv(1024)
     # parse data
+
     # crack 3 times
+    #use crack()
 
 if __name__ == "__main__":
     server_crack()
